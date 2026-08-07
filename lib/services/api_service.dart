bbc4f4640160b8204677d4a751c18af945569a13
Arman Maulana <azkaowi@gmail.com>
2026-08-07 23:13:29 +0700

    edit service api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,8 +1,8 @@
-// class ApiService {
-//   static const baseUrl =
-//       'https://backend-ticket-kcc-h3c4cehsdwh8gna8.indonesiacentral-01.azurewebsites.net/api';
-// }
-
 class ApiService {
-  static const baseUrl = 'https://a27f-182-2-191-20.ngrok-free.app/api';
+  static const baseUrl =
+      'https://backend-ticket-kcc-h3c4cehsdwh8gna8.indonesiacentral-01.azurewebsites.net/api';
 }
+
+// class ApiService {
+//   static const baseUrl = 'https://a27f-182-2-191-20.ngrok-free.app/api';
+// }
